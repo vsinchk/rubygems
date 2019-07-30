c2ec2e18f0badf104cdb28abdc77571e9bbc480f
Hiroshi SHIBATA <hsbt@ruby-lang.org>
2019-07-30 17:15:49 +0800

    make enabble as default for conservative option

--- lib/rubygems/install_update_options.rb
+++ lib/rubygems/install_update_options.rb
@@ -186,6 +186,8 @@ module Gem::InstallUpdateOptions
   def install_update_options
     {
       :document => %w[ri],
+      :conservative => true,
+      :minimal_deps => true,
     }
   end
 
@@ -193,7 +195,7 @@ module Gem::InstallUpdateOptions
   # Default description for the gem install and update commands.
 
   def install_update_defaults_str
-    "--document=ri"
+    "--document=ri --conservative"
   end
 
 end

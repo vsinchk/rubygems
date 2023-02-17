22b9070e97f3da5be4285d654dcfbee46d60aaa7
Mercedes Bernard <mercedesrbernard@gmail.com>
2023-02-17 16:59:52 -0600

    make checksums an array to support future hashing algos

--- bundler/lib/bundler/lockfile_parser.rb
+++ bundler/lib/bundler/lockfile_parser.rb
@@ -202,7 +202,7 @@ module Bundler
 
         version = Gem::Version.new(version)
         platform = platform ? Gem::Platform.new(platform) : Gem::Platform::RUBY
-        checksum = Bundler::Checksum.new(name, version, platform, checksum)
+        checksum = Bundler::Checksum.new(name, version, platform, [checksum])
         @checksums[checksum.full_name] = checksum
       end
     end

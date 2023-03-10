35a58dbb6bfa723d0fd8fb7ae76b5dbcfea3406e
Hiroshi SHIBATA <hsbt@ruby-lang.org>
2023-03-10 16:01:47 +0900

    Also fix test_gem_commands_update_command.rb

--- test/rubygems/test_gem_commands_update_command.rb
+++ test/rubygems/test_gem_commands_update_command.rb
@@ -670,8 +670,10 @@ class TestGemCommandsUpdateCommand < Gem::TestCase
 
     expected = {
       :args => [],
+      :conservative => true,
       :document => %w[ri],
       :force => false,
+      :minimal_deps => true,
       :system => true,
     }
 
@@ -689,8 +691,10 @@ class TestGemCommandsUpdateCommand < Gem::TestCase
 
     expected = {
       :args => [],
+      :conservative => true,
       :document => %w[ri],
       :force => false,
+      :minimal_deps => true,
       :system => "1.3.7",
     }
 
